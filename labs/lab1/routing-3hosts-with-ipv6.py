cb75a3cdf57a48229a7106370fc8267c4f5c8a38
Stefano Salsano <stefano.salsano@uniroma2.it>
2022-12-03 17:30:14 +0000

    routing-3hosts-with-ipv6.py created online with Bitbucket

---
+++ labs/lab1/routing-3hosts-with-ipv6.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False )
+
+    info( '*** Adding controller\n' )
+    info( '*** Add switches\n')
+    r1 = net.addHost('r1', cls=Node, ip=None)
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip=None, mac='00:00:10:00:00:64')
+    h2 = net.addHost('h2', cls=Host, ip=None, mac='00:00:10:00:01:64')
+    h3 = net.addHost('h3', cls=Host, ip=None, mac='00:00:10:00:02:64')
+
+   
+    info( '*** Add links\n')
+    net.addLink(h1, r1)
+    net.addLink(h2, r1)
+    net.addLink(h3, r1)
+
+
+    info( '*** Starting network\n')
+    net.build()
+
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    r1.intf( 'r1-eth0' ).config(mac='00:00:10:00:00:01')
+    r1.intf( 'r1-eth1' ).config(mac='00:00:10:00:01:01')
+    r1.intf( 'r1-eth2' ).config(mac='00:00:10:00:02:01')
+
+
+    info( '*** Post configure switches and hosts\n')
+
+    r1.cmd('sysctl -w net.ipv6.conf.all.forwarding=1')
+
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
